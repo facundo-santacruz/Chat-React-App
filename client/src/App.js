691e05128cd8d5856e74c1e06e34834806bbae69
facundo-santacruz <facugs2090@gmail.com>
2021-02-02 03:55:15 -0300

    Adding react-router-dom dependency and setting App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  useLocation,
+}from "react-router-dom";
+import Login from "./components/Login";
+import AddRoom from "./components/AddRoom";
+import ChatRoom from "./components/ChatRoom";
+import RoomList from "./components/RoomList";
 
 function App() {
+let location = useLocation();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Redirect
+          to={{
+            pathname:"/roomList",
+            state: { from: location }
+          }}
+        />
+        <Switch>
+          <Route path="/login" component={Login} />
+          <SecureRoute path="/roomList" component={RoomList} />
+          <SecureRoute path="/addRoom" component={AddRoom} />
+          <SecureRoute path="/chatRoom/:room" component={ChatRoom} />
+        </Switch>
+      </div>
+
+      </Router>
   );
 }
 
 export default App;
+
+function SecureRoute({ children, ...rest }) {
+  return (
+    <Route 
+      {...rest}
+      render={({ location }) =>
+        localStorage.get('nickname') ? (
+          children 
+        ) : (
+          <Redirect
+          to={{
+            pathname: "/login",
+            state: { from: location}
+          }}
+          />
+        )
+      }
+    />
+  )
+}
